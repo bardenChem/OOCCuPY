30bbf6cad2ab7d465282388f2fc4fdba4f3094f1
bardenChem <primordia.lqqc@gmail.com>
2026-07-02 20:26:51 -0300

    k

--- pDynamoWrapper/ReactionCoordinate.py
+++ pDynamoWrapper/ReactionCoordinate.py
@@ -191,12 +191,7 @@ class ReactionCoordinate:
                     print(f"DEBUG: d(A-B) = {self.AB:.3f}, d(B-C) = {self.BC:.3f}")
                     print(f"DEBUG: weight13 = {self.weight13:.3f}, weight31 = {self.weight31:.3f}")
                     print(f"DEBUG: Calculated RC = {self.minimumD:.3f}")
-                    print(f"DEBUG: Expected RC = {self.AB - self.BC:.3f}")
-                    x0, y0, z0 = system.coordinates3[self.atoms[0]]
-                    x1, y1, z1 = system.coordinates3[self.atoms[1]]
-                    x2, y2, z2 = system.coordinates3[self.atoms[2]]                    
-                    print(f"Coordinates atom 1 = ({x0:.3f}, {y0:.3f}, {z0:.3f}), atom 2 = ({x1:.3f}, {y1:.3f}, {z1:.3f}), atom 3 = ({x2:.3f}, {y2:.3f}, {z2:.3f})")
-
+                    print(f"DEBUG: Expected RC = {self.AB - self.BC:.3f}")                           
                     # For transfer, we scan from initial to final state
                     self.reaction_type = "transfer"
                     self.SetTransferLimits()
